80f33948cf8aa1d5e953cf164fe8efdd9eb49c76
Eugene Selivonchyk <leto41ogo@yahoo.com>
2016-08-07 23:59:42 +0200

    Output improvements: checkpoints and visualization

--- model/doom_basic.py
+++ model/doom_basic.py
@@ -10,73 +10,89 @@ import numpy as np
 import utils as ut
 import input as inp
 import activation_functions as act
+import itertools
 
 import prettytensor as pt
+import prettytensor.bookkeeper as bookkeeper
 from prettytensor.tutorial import data_utils
-
-tf.app.flags.DEFINE_string(
-    'save_path', None, 'Where to save the model checkpoints.')
-FLAGS = tf.app.flags.FLAGS
 
 
 BATCH_SIZE = 20
 EPOCH_SIZE = 60000 // BATCH_SIZE
 TEST_SIZE = 10000 // BATCH_SIZE
 
-HIDDEN_0_SIZE = 200
-HIDDEN_1_SIZE = 100
+LAYER_NARROW = 10
+LAYER_ENCODER = 500
+LAYER_DECODER = 500
+
 layer_info = []     # saves info about layers
 
 IMAGE_SHAPE = None
 BATCH_SHAPE = None
 
+
+tf.app.flags.DEFINE_string(
+    'save_path', './tmp/checkpoint', 'Where to save the model checkpoints.')
+tf.app.flags.DEFINE_string(
+    'logdir', '', 'where to save logs.')
 tf.app.flags.DEFINE_string('model', 'full',
                            'Choose one of the models, either full or conv')
 tf.app.flags.DEFINE_string('input_folder',
-                           '../data/tmp/circle_pos_delay_1/img/',
+                           '../data/tmp/circle_pos_delay_3/img/',
                            'Choose input source folder')
 tf.app.flags.DEFINE_float("learning_rate", 1e-2, "learning rate")
 tf.app.flags.DEFINE_boolean("interactive", True, "use interactive interface")
-
+tf.app.flags.DEFINE_boolean("load state", False, "load existing model state")
+FLAGS = tf.app.flags.FLAGS
 
 FLAGS = tf.app.flags.FLAGS
 
 
 def encoder(input_tensor):
     global layer_info
-    layer_info.append(HIDDEN_0_SIZE)
+    layer_info.append(LAYER_NARROW)
     return (pt.wrap(input_tensor)
             .flatten()
-            .fully_connected(HIDDEN_0_SIZE)
+            .fully_connected(LAYER_NARROW)
             ).tensor
 
 
 def encoder_2(input_tensor):
     global layer_info
-    layer_info.append(HIDDEN_0_SIZE)
-    layer_info.append(HIDDEN_1_SIZE)
+    layer_info.append(LAYER_ENCODER)
+    layer_info.append(LAYER_NARROW)
 
     return (pt.wrap(input_tensor)
             .flatten()
-            .fully_connected(HIDDEN_1_SIZE)
-            .fully_connected(HIDDEN_0_SIZE).tensor)
+            .fully_connected(LAYER_ENCODER)
+            .fully_connected(LAYER_NARROW))
 
 
 def encoder_conv(input_tensor):
     global layer_info
-    layer_info.append(HIDDEN_0_SIZE)
-    layer_info.append(HIDDEN_1_SIZE)
+    layer_info.append(LAYER_ENCODER)
+    layer_info.append(LAYER_NARROW)
 
     return (pt.wrap(input_tensor)
             .flatten()
-            .fully_connected(HIDDEN_1_SIZE)
-            .fully_connected(HIDDEN_0_SIZE).tensor)
+            .fully_connected(LAYER_ENCODER)
+            .fully_connected(LAYER_NARROW))
+
 
 def decoder(input_tensor=None, weight_init=tf.truncated_normal):
     return (pt.wrap(input_tensor)
             .fully_connected(
         IMAGE_SHAPE[0]*IMAGE_SHAPE[1]*IMAGE_SHAPE[2],
         init=weight_init))
+
+
+def decoder_2(input_tensor=None, weight_init=tf.truncated_normal):
+    layer_info.append(LAYER_DECODER)
+    return (pt.wrap(input_tensor)
+        .fully_connected(LAYER_DECODER)
+        .fully_connected(IMAGE_SHAPE[0]*IMAGE_SHAPE[1]*IMAGE_SHAPE[2],
+        init=weight_init))
+
 
 def get_reconstruction_cost(output_tensor, target_tensor, epsilon=1e-8):
     target_tensor = (pt.wrap(target_tensor).flatten()).tensor
@@ -112,7 +128,6 @@ def fetch_datasets(data_min, data_max, source_folder):
     TEST_SIZE  = len(input_source) // BATCH_SIZE
 
     input_source = ut.rescale_ds(input_source, data_min, data_max)
-
     return input_source, input_source, input_source[0:BATCH_SIZE]
 
 
@@ -121,6 +136,12 @@ def get_batch_shape():
         return BATCH_SIZE, IMAGE_SHAPE[0], IMAGE_SHAPE[1], IMAGE_SHAPE[2]
     else:
         return BATCH_SIZE, IMAGE_SHAPE[0], IMAGE_SHAPE[1], [1]
+
+
+def encode_data(embeddings, input_placeholder, output_placeholder, sess, train_data):
+    feed = zip(xrange(EPOCH_SIZE), pt.train.feed_numpy(BATCH_SIZE, train_data, train_data))
+    result_batches = [sess.run([embeddings], dict(zip((input_placeholder, output_placeholder), data)))[0] for _, data in feed]
+    return np.vstack(result_batches)
 
 
 vizualize_intermidiate = 10
@@ -132,15 +153,19 @@ def main(_=None,
          epochs=5,
          learning_rate=0.01,
          source_folder=FLAGS.input_folder,
-         enc=encoder,
-         dec=decoder):
+         enc=encoder_2,
+         dec=decoder_2,
+         optimizer=tf.train.AdamOptimizer):
 
     tf.reset_default_graph()
 
     global IMAGE_SHAPE, layer_info
-    best_q, layer_info = None, []
+    accuracy_by_epoch, layer_info = [], []
     epoch_reconstruction = []
     IMAGE_SHAPE = inp.get_image_shape(source_folder)
+    meta = {'suf': 'doom_bs', 'act': activation.func, 'e': epochs, 'lr': learning_rate,
+            'init': weight_init, 'bs': BATCH_SIZE, 'h': layer_info, 'opt': optimizer}
+    ut.configure_folders(FLAGS, meta)
 
     input_placeholder  = tf.placeholder(tf.float32, get_batch_shape())
     output_placeholder = tf.placeholder(tf.float32, get_batch_shape())
@@ -156,17 +181,24 @@ def main(_=None,
                            ):
         with pt.defaults_scope(phase=pt.Phase.train):
             with tf.variable_scope("model") as scope:
-                output_tensor = decoder(encoder(input_placeholder), weight_init=weight_init)
+                encode_op = enc(input_placeholder)
+                output_tensor = dec(encode_op, weight_init=weight_init)
 
     pretty_loss = square_loss(output_tensor, output_placeholder)
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    optimizer = optimizer(learning_rate=learning_rate)
     train = pt.apply_optimizer(optimizer, losses=[pretty_loss])
 
     init = tf.initialize_all_variables()
-    runner = pt.train.Runner(save_path=FLAGS.save_path)
+    runner = pt.train.Runner(save_path=FLAGS.save_path, logdir=FLAGS.logdir)
     with tf.Session() as sess:
         sess.run(init)
+
+        if FLAGS.load_state:
+            runner.load_from_checkpoint(sess)
+            epochs_past = bookkeeper.global_step().eval() / EPOCH_SIZE
+            ut.print_info('Checkpoint restore requested. previous epoch: %d' % epochs_past, color=31)
+
         for epoch in xrange(epochs):
             epoch_info = ''
 
@@ -190,41 +222,59 @@ def main(_=None,
                 feed_vars=(input_placeholder, output_placeholder),
                 feed_data=pt.train.feed_numpy(BATCH_SIZE, test_data, test_data))
             ut.print_time('Accuracy after %2d/%d epoch %.2f; %s' % (epoch + 1, epochs, accuracy, epoch_info))
-            if best_q is None or best_q > accuracy:
-                best_q = accuracy
+            accuracy_by_epoch.append(accuracy)
 
-        save_params = {'suf': 'doom_bs', 'act': activation.func, 'e': epochs, 'opt': optimizer, 'lr': learning_rate,
-                       'init': weight_init, 'acu': int(best_q), 'bs': BATCH_SIZE, 'h': layer_info}
-        ut.reconstruct_images_epochs(np.asarray(epoch_reconstruction), visualization_data, save_params=save_params, img_shape=IMAGE_SHAPE)
+        checkpoint(sess, runner, encode_op, input_placeholder, output_placeholder, train_data)
 
-    ut.print_time('Best Quality: %f for %s' % (best_q, ut.to_file_name(save_params)))
-    return best_q
+        meta['acu'] = int(np.min(accuracy_by_epoch))
+        ut.reconstruct_images_epochs(np.asarray(epoch_reconstruction), visualization_data, save_params=meta,
+                                     img_shape=IMAGE_SHAPE)
+
+    ut.print_time('Best Quality: %f for %s' % (np.min(accuracy_by_epoch), ut.to_file_name(meta)))
+    return meta, accuracy_by_epoch
 
 
-def search_learning_rate(lrs=[100, 5, 0.5, 0.1, 0.01, 0.001, 0.0001], enc=encoder,dec=decoder, epochs=50):
-    best_q, best_r = None, None
-    res = []
+def checkpoint(sess, runner, encode_op, input_placeholder, output_placeholder, train_data):
+    runner._saver.save(sess, FLAGS.save_path, 999)
+    embeddings = encode_data(encode_op, input_placeholder, output_placeholder, sess, train_data)
+    epochs_past = bookkeeper.global_step().eval() / EPOCH_SIZE
+    projection_file = ut.to_file_name({'suf': 'emb', 'e': int(epochs_past)}, FLAGS.save_path, 'txt')
+    np.savetxt(projection_file, embeddings)
+
+
+def search_learning_rate(lrs=[100, 5, 0.5, 0.1, 0.01, 0.001, 0.0001, 0.00001], enc=encoder,dec=decoder, epochs=50):
+    result_best, arg_best = None, None
+    result_summary = []
+    result_list = []
     for lr in lrs:
-        q = main(learning_rate=lr, epochs=epochs, enc=enc, dec=dec)
-        res.append('\n\r lr:%.4f \tq:%.2f' % (lr, q))
-        if best_q is None or best_q > q:
-            best_q = q
-            best_r = lr
-    print(''.join(res))
-    ut.print_info('BEST Q: %d IS ACHIEVED FOR LR: %f' %(best_q, best_r), 36)
+        meta, accuracy_by_epoch = main(learning_rate=lr, epochs=epochs, enc=enc, dec=dec)
+        result_list.append((ut.to_file_name(meta), accuracy_by_epoch))
+        best_accuracy = np.min(accuracy_by_epoch)
+        result_summary.append('\n\r lr:%2.5f \tq:%.2f' % (lr, best_accuracy))
+        if result_best is None or result_best > best_accuracy:
+            result_best = best_accuracy
+            arg_best = lr
+    meta = {'suf': 'grid_doom_bs', 'e': epochs, 'lrs': lrs, 'enc': enc, 'dec': dec, 'acu': result_best,
+            'bs': BATCH_SIZE, 'h': layer_info}
+    ut.plot_log(meta, result_list)
+    print(''.join(result_summary))
+    ut.print_info('BEST Q: %d IS ACHIEVED FOR LR: %f' %(result_best, arg_best), 36)
 
+
+def configure_for_cluster():
+    pass
 
 def try_this():
     # identity learning
     pass
 
-search_learning_rate()
-search_learning_rate(enc=encoder_2, epochs=500)
+# search_learning_rate()
+# search_learning_rate(lrs=[0.03, 0.001, 0.0001, 0.00001], enc=encoder_2, epochs=50)
+FLAGS.load_state = True
+main(learning_rate=0.0001, epochs=3)
 
-# main(learning_rate=0.001, epochs=5)
-
+# main(learning_rate=0.0001, epochs=1000)
+# main(learning_rate=0.0004, epochs=100)
 # imgs = np.asarray(inp.get_images(FLAGS.input_folder))/255.0
-#
 # ut.reconstruct_images_epochs(None, imgs[0:10], save_params={'test':'test'}, img_shape=imgs[0].shape)
-
 # current best sigmoid (0, 1) lr=0.001
--- model/utils.py
+++ model/utils.py
@@ -7,7 +7,9 @@ import tensorflow as tf
 from scipy import misc
 
 
-image_folder = './img/'
+IMAGE_FOLDER = './img/'
+EPOCH_THRESHOLD = 4
+
 _start_time = None
 
 
@@ -70,13 +72,16 @@ def rescale_ds(ds, min, max):
 
 
 def _save_image(name='image', save_params=None, image=None):
+    if save_params is not None and 'e' in save_params and save_params['e'] < EPOCH_THRESHOLD:
+        print_info('IMAGE: output is not saved. epochs %d < %d' % (save_params['e'], EPOCH_THRESHOLD), color=31)
+        return
+
     file_name = name if save_params is None else to_file_name(save_params)
     file_name += '.png'
-    name = os.path.join(image_folder, file_name)
-
+    name = os.path.join(IMAGE_FOLDER, file_name)
 
     if image is not None:
-        misc.imsave(os.path.join(image_folder, file_name), arr=image, format='png')
+        misc.imsave(name, arr=image, format='png')
     #
     # plt.savefig(name, dpi=300, facecolor='w', edgecolor='w',
     #             orientation='portrait', papertype=None, format=None,
@@ -139,7 +144,7 @@ def reconstruct_images_epochs(epochs, original=None, save_params=None, img_shape
     full_picture = None
     img_shape = img_shape if img_shape is not None else _construct_img_shape(epochs[0][0])
 
-    if original is not None and epochs is not None:
+    if original is not None and epochs is not None and len(epochs) >= 3:
         min_ref, max_ref = np.min(original), np.max(original)
         print_info('epoch avg: (original: %s) -> %s' % (str(np.mean(original)), str((np.mean(epochs[0]), np.mean(epochs[1]), np.mean(epochs[2])))))
         print_info('reconstruction char. in epochs (min, max)|original: (%f %f)|(%f %f)' % (np.min(epochs[1:]), np.max(epochs), min_ref, max_ref))
@@ -153,14 +158,21 @@ def reconstruct_images_epochs(epochs, original=None, save_params=None, img_shape
         for _, epoch in enumerate(epochs):
             full_picture = concat_images(full_picture, _reconstruct_picture_line(epoch, img_shape), axis=1)
     if original is not None:
-        print('original shape', original[0].shape)
+        # print('original shape', original[0].shape)
         full_picture = concat_images(full_picture, _reconstruct_picture_line(original, img_shape), axis=1)
 
     _show_picture(full_picture)
     _save_image(save_params=save_params, image=full_picture)
 
 
-def to_file_name(obj):
+def _abbreviate_string(value):
+    abbr = [letter for letter in value if letter.isupper()]
+    if len(abbr) > 1:
+        return ''.join(abbr)
+    return value
+
+
+def to_file_name(obj, folder=None, ext=None):
     name = ''
     od = collections.OrderedDict(sorted(obj.items()))
 
@@ -177,20 +189,20 @@ def to_file_name(obj):
                 value = ''.join(list(map(lambda x: x.upper()[0], parts)))
         elif ' at ' in str(value):
             value = (str(value).split()[0]).split('.')[-1]
-            abbr = [letter for letter in value if letter.isupper()]
-            if len(abbr) > 1:
-                value = ''.join(abbr)
+            value = _abbreviate_string(value)
+        elif isinstance(value, type):
+            value = _abbreviate_string(value.__name__)
 
-        if isinstance(value, float):
+        if isinstance(value, float) or isinstance(value, np.float32):
             value = '%.4f' % value
 
         if isinstance(value, int):
-            value = '%2d' % value
+            value = '%02d' % value
 
         if isinstance(value, list):
-            value = '|'.join([str(i) for i in value])
+            value = '|'.join(map(str, value))
 
-        if len(value) > 8:
+        if len(value) > 9:
             print_info('truncating this: %s %s' % (key, value))
             value = value[0:9]
 
@@ -198,6 +210,11 @@ def to_file_name(obj):
             name = str(value) + name
         else:
             name += '__%s|%s' % (key, str(value))
+
+    if folder:
+        name = os.path.join(folder, name)
+    if ext:
+        name += '.' + ext
     return name
 
 
@@ -207,3 +224,28 @@ def print_model_info():
         print(v.name, v.get_shape())
     for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
         print(v.name, v.get_shape())
+
+
+def plot_log(meta, data):
+    name = IMAGE_FOLDER + '/grid_lr_' + to_file_name(meta) + '.png'
+
+    x = np.arange(0, len(data[0][1])) + 1
+    for _, experiment in enumerate(data):
+        plt.semilogy(x, experiment[1], label=experiment[0], marker='o', linestyle='--')
+    plt.xlim([1, x[-1]])
+    plt.legend(loc='best', fancybox=True, framealpha=0.5, fontsize=8)
+    plt.savefig(name, dpi=300, facecolor='w', edgecolor='w',
+            transparent=False, bbox_inches='tight', pad_inches=0.1,
+            frameon=None)
+
+
+def configure_folders(FLAGS, meta):
+    folder_name = to_file_name(meta) + '/'
+    full_path = os.path.join('./tmp/', folder_name)
+    if not os.path.exists(full_path):
+        os.mkdir(full_path)
+    log_path = os.path.join(full_path, 'log')
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+    FLAGS.save_path = full_path
+    FLAGS.logdir = log_path
