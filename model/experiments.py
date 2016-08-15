c82941ac9c902659c88ffd03d2a1cab881e76327
Eugene Selivonchyk <leto41ogo@yahoo.com>
2016-08-15 22:31:03 +0200

    Layer sizes grid search

--- model/experiments.py
+++ model/experiments.py
@@ -30,6 +30,34 @@ def search_learning_rate(lrs=[0.003, 0.001, 0.0004, 0.0001, 0.00003, 0.00001],
   ut.print_info('BEST Q: %d IS ACHIEVED FOR LR: %f' % (best_result, best_args), 36)
 
 
+def search_layer_sizes(epochs=200):
+  best_result, best_args = None, None
+  result_summary, result_list = [], []
+
+  for _, h_encoder in enumerate[100, 500, 2000]:
+    for _, h_decoder in enumerate[100, 500, 2000]:
+      for _, h_narrow in enumerate[3, 6, 12]:
+        model = dm.DoomModel()
+        model.layer_encoder = h_encoder
+        model.layer_narrow = h_narrow
+        model.layer_decoder = h_decoder
+        layer_info = str(model.get_layer_info())
+
+        meta, accuracy_by_epoch = model.train(epochs)
+        result_list.append((layer_info, accuracy_by_epoch))
+        best_accuracy = np.min(accuracy_by_epoch)
+        result_summary.append('\n\r h:%s \tq:%.2f' % (layer_info, best_accuracy))
+        if best_result is None or best_result > best_accuracy:
+          best_result = best_accuracy
+          best_args = layer_info
+
+  meta = {'suf': 'grid_doom_bs', 'e': epochs, 'acu': best_result,
+          'bs': FLAGS.batch_size, 'h': model.get_layer_info()}
+  print(''.join(result_summary))
+  ut.print_info('BEST Q: %d IS ACHIEVED FOR LR: %f' % (best_result, best_args), 36)
+  ut.plot_epoch_progress(meta, result_list)
+
+
 def print_reconstructions_along_with_originals():
   FLAGS.load_from_checkpoint = './tmp/doom_bs__act|sigmoid__bs|20__h|500|5|500__init|na__inp|cbd4__lr|0.0004__opt|AO'
   model = dm.DoomModel()
